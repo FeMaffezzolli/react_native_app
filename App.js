accee7b82bc6ee32b644dd5e2fbd78f817a21303
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2019-01-31 21:17:53 -0200

    Listing names

--- App.js
+++ App.js
@@ -4,10 +4,26 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header';
 
 export default class App extends React.Component {
+
+  renderList() {
+    const names = [
+      'Norah Jones',
+      'Pearl Jam',
+      'Nirvana'
+    ];
+
+    const textElements = names.map(name => {
+      return <Text>{name}</Text>
+    })
+
+    return textElements;
+  }
+
   render() {
     return (
       <View>
         <Header title="People" />
+        { this.renderList() }
       </View>
     );
   }
