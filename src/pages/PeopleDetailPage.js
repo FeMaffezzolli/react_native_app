6249bac2cd016ee18d559edb1e1155ac7be63633
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2019-02-05 19:19:21 -0200

    Created Line component

--- src/pages/PeopleDetailPage.js
+++ src/pages/PeopleDetailPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, View, Image } from 'react-native'
+import Line from '../components/Line';
 
 export default class PeopleDetailPage extends Component {
   render() {
@@ -7,16 +8,14 @@ export default class PeopleDetailPage extends Component {
 
     return (
       <View style={styles.container}>
-          <Image
-            style={styles.avatar}
-            source={{ uri: person.picture.large }}
-          />
-          <View style={styles.detailContainer}>
-            <View style={styles.line}>
-                <Text style={[styles.cell, styles.label]}>Email: </Text>
-                <Text style={styles.cell}> {person.email} </Text>
-            </View>
-          </View>
+            <Image
+                style={styles.avatar}
+                source={{ uri: person.picture.large }}
+            />
+        <View style={styles.detailContainer}>
+            <Line label="Email:" content={person.email} />
+            <Line label="Cidade:" content={person.location.city} />
+        </View>
       </View>
     );
   }
@@ -33,20 +32,5 @@ const styles = StyleSheet.create({
         backgroundColor: '#e2f9ff',
         marginTop      : 20,
         elevation      : 1
-    },
-    line: {
-        flexDirection: 'row',
-        paddingTop   : 3,
-        paddingBottom : 3,
-        borderWidth: 1,
-        borderColor: '#C5C5C5'
-    },
-    cell: {
-        fontSize: 16,
-        paddingLeft: 5,
-        // borderWidth: 1,
-    },
-    label: {
-        fontWeight: 'bold',
     }
 })
